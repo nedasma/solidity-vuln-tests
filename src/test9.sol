a309ad7e927d9720fc82caa0330f3bbd7df31fce
Nedas Matulevicius <39617591+nedasma@users.noreply.github.com>
2021-10-14 16:50:09 +0300

    Modified test to fit the latest solc version

--- src/test9.sol
+++ src/test9.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >0.4.16 <=0.7.0;
+pragma solidity >0.4.16 <=0.9.0;
 
 contract SimpleStorage {
 	uint storedData;
@@ -9,7 +9,7 @@ contract SimpleStorage {
 	}
 	
 	function get(uint _data) public payable {
-		msg.sender.transfer(_data);
+		payable(msg.sender).transfer(_data);
 	}
 	
 }
